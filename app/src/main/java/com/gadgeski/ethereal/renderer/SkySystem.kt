9c7b507a27654bf8feff2d455b72363d542c3603
gadgeski <xtwh.ce5bil@gmail.com>
2026-02-15 21:05:52 +0900

    fix:Image updates and minor display fixes

--- app/src/main/java/com/gadgeski/ethereal/renderer/SkySystem.kt
+++ app/src/main/java/com/gadgeski/ethereal/renderer/SkySystem.kt
@@ -22,7 +22,7 @@ class SkySystem(context: Context) {
 
     private var xOffset: Float = 0f
     private val parallaxFactor = 0.3f
-    private val scale = 1.4f
+    private val scale = 1.3f
 
     init {
         val original = BitmapFactory.decodeResource(context.resources, R.drawable.ethereal_bg)
